98182cd7cea095cf4118a69d0a1d6a875e5c5a84
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-07-22 00:51:23 +0600

    Homework 4
    add

---
+++ JavaHomework5/src/javahomework5/JavaHomework5.java
@@ -0,0 +1,62 @@
+
+package javahomework5;
+
+import java.util.Scanner;
+
+
+public class JavaHomework5 {
+
+   
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        
+//    1. Program asks for a user name. The user will input a name. Program print Welcome ‘user
+//name’.
+
+//        
+//        
+//        System.out.println("Enter your Name: ");
+//       
+//         String userName =input.nextLine();
+//        
+//        System.out.println("Welcom " +userName);
+        
+//        2. Program as for input two numbers. The user will input two numbers. Program print the
+//summation of two numbers.
+      
+
+//        System.out.println("Entre first number: ");
+//       int number1 = input.nextInt();
+//       
+//        System.out.println("Entre 2nd number: ");
+//        int number2 = input.nextInt();
+//       
+//        int sum=  number1+ number2 ;
+//        System.out.println("The summation of two number is " + sum);
+
+//      
+//4. Program asks for Marks. The user will input a Marks.
+//Program print Your Mark is ‘Marks’
+
+//3. Program as for input two numbers. The user will input two numbers. Program print the
+//Modulus of two numbers
+
+        System.out.println("Entre first number ");
+        int number1= input.nextInt();
+        
+        
+        System.out.println("Enter second number");
+        int number2 = input.nextInt();
+        
+        int modulus =number1%number2;
+        
+        System.out.println("The modulus is "+ modulus);
+    
+//        System.out.println("Entre a number ");
+//        
+//        int marks = input.nextInt();
+//        System.out.println("The number is "+ marks);
+    
+    }
+    
+}
