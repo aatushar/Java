7c3ff9cb103ab41984d19417806c75f03b652ae2
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-07-28 23:59:13 +0600

    27 july
    add

---
+++ PraticeForMCQ/src/praticeformcq/PraticeForMCQ.java
@@ -0,0 +1,23 @@
+
+package praticeformcq;
+
+
+public class PraticeForMCQ {
+
+   
+    public static void main(String[] args) {
+       
+        
+        
+        System.out.println("Tushar ");
+        System.out.println("PrioToma ");
+        
+        
+        System.out.print("Hridoy");
+        System.out.print("Mamun");
+        
+        
+        
+    }
+    
+}
