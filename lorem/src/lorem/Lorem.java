2ea85ade2764273bd1176b6df4c73ae6ba6d83de
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-08-12 13:08:17 +0600

    12 august
    add

--- lorem/src/lorem/Lorem.java
+++ lorem/src/lorem/Lorem.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lorem;
 
-/**
- *
- * @author user
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Lorem {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\user\\Desktop\\lorem.txt");
+        Scanner read = new Scanner(file);
+//        PrintWriter pw = new PrintWriter(file);
+        
+        
+       
+        
+       if (file.exists()) {
+            String tenWords = "";
+        int count = 0;
+            while (read.hasNext()) {
+                String word = read.next();
+                tenWords +=word+" ";
+                count++;
+                if (count == 10) {
+
+                    System.out.println(tenWords);
+                    count=0;
+                    tenWords="";
+                }
+                
+        }
+
     }
-    
+    }
 }
+
