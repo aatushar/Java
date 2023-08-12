4e36af912fd03c849ecc5e96bed17f63e2192d30
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-08-13 01:01:14 +0600

    12 august
    add

--- fileAndTextHomework/src/fileandtexthomework/FileAndTextHomework.java
+++ fileAndTextHomework/src/fileandtexthomework/FileAndTextHomework.java
@@ -25,7 +25,6 @@ public class FileAndTextHomework {
                     tenWords= " ";
                     
                 }
-//                System.out.println(tenWords);
             }
 
         }
