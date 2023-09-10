fb4eccaddef2cc29455f60c18123e6a05ab67be9
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-09-10 13:02:39 +0600

    add
    10 sept 2023

---
+++ EVDWriteIntoFile/src/evdwriteintofile/EVDWriteIntoFile.java
@@ -0,0 +1,15 @@
+
+package evdwriteintofile;
+
+import view.From;
+
+public class EVDWriteIntoFile {
+
+   
+    public static void main(String[] args) {
+        From f= new From();
+       f.setLocationRelativeTo(null);
+       f.setVisible(true);
+    }
+    
+}
