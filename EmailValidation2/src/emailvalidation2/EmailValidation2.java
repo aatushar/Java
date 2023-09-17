3acaeccb42bdfa76b79e99d2a8c91de35c9d804d
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-09-17 12:47:36 +0600

    Add
    Add

---
+++ EmailValidation2/src/emailvalidation2/EmailValidation2.java
@@ -0,0 +1,12 @@
+
+package emailvalidation2;
+
+
+public class EmailValidation2 {
+
+    
+    public static void main(String[] args) {
+      
+    }
+    
+}
---
+++ StudentRegForm/src/studentregform/StudentRegForm.java
@@ -0,0 +1,17 @@
+
+package studentregform;
+
+import pak.Form;
+
+
+public class StudentRegForm {
+
+ 
+    public static void main(String[] args) {
+        Form f= new Form();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        
+    }
+    
+}
