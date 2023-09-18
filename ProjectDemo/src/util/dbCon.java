8886feaa617853b46de7a8b376634eab43283a78
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-09-19 00:58:20 +0600

    Add
    Add

--- ProjectDemo/src/util/dbCon.java
+++ ProjectDemo/src/util/dbCon.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package util;
 
-/**
- *
- * @author LENOVO
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Logger;
+
+
 public class dbCon {
+    
+    private  Connection con = null;
+    private String url="jdbc:mysql://localhost:3306/advancejava";
+    private String user="root";
+    private String password="1234";
+    private String driver="com.mysql.cj.jdbc.Driver";
+    
+    public Connection getCon(){
+        try {
+            Class.forName(driver);
+            con=DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DbConect.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbConect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
     
 }
