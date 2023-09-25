6ae96da81af2cd95e66001f4a23923ec79918b36
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-09-25 13:32:13 +0600

    Add
    Add

---
+++ DbTransferAndTableCreation/src/dbtransferandtablecreation/DbTransferAndTableCreation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dbtransferandtablecreation;
+
+/**
+ *
+ * @author user
+ */
+public class DbTransferAndTableCreation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
