c6c3137f3dcc07283f6534ecdc6e0707768cf598
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-10-02 01:47:29 +0600

    Add
    Add

---
+++ EcoomerceManagementSystem/src/ecoomercemanagementsystem/EcoomerceManagementSystem.java
@@ -0,0 +1,19 @@
+
+package ecoomercemanagementsystem;
+
+import view.LoginForm;
+
+
+public class EcoomerceManagementSystem {
+
+  
+    public static void main(String[] args) {
+       
+        
+        LoginForm Login = new LoginForm();
+        Login.setVisible(true);
+        Login.pack();
+        Login.setLocationRelativeTo(null);
+    }
+    
+}
